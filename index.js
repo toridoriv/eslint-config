ab20fbe0828b1cb5b6936f6c09a743af97e19c2a
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-02-24 18:01:19 -0300

    style: Fix format issues

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as prettierConfig } from "./lib/prettier/index.js";
 export { default as eslintConfig } from "./lib/eslint/index.js";
+export { default as prettierConfig } from "./lib/prettier/index.js";

c8704fad49b168375f550623e953c5a2447ab2f9
Victoria Rodriguez <vrodriguezfe@icloud.com>
2024-09-23 15:00:38 -0300

    :wrench: Update development config files

--- eslint.config.js
+++ eslint.config.js
@@ -6,10 +6,26 @@ eslintConfig.javascript.node[1]?.files?.push("bin/setup-eslint-config");
  * @type {import("eslint").Linter.FlatConfig[]}
  */
 export default [
-  ...eslintConfig.ignorePatterns,
+  {
+    ignores: [
+      "DS_Store",
+      "node_modules",
+      "package-lock.json",
+      "coverage",
+      "!bin/**",
+      "!.vscode/**",
+      "logs",
+      "*.log",
+      "npm-debug.log*",
+      "tmp/",
+      "*.tmp",
+      "tmp.*",
+      "var/tmp",
+    ],
+  },
   ...eslintConfig.javascript.node,
-  ...eslintConfig.jsdoc,
   ...eslintConfig.typescript,
+  ...eslintConfig.jsdoc,
   ...eslintConfig.json,
   ...eslintConfig.markdown,
   ...eslintConfig.prettier,

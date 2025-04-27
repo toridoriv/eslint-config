33471d924d7fdd2d947aabccb9586dcec10156d1
Victoria Rodriguez <vrodriguezfe@icloud.com>
2025-04-26 20:14:22 -0400

    :bug: Fix typings file to correctly export eslintConfig and prettierConfig

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,9 @@
-export * from "./index.js";
+/* eslint-disable prettier/prettier */
+export type * from "./lib/eslint/index.js";
+export type * from "./lib/prettier/index.js";
+
+import * as eslint from "./lib/eslint/index.js";
+import * as prettier  from "./lib/prettier/index.js";
+
+export const eslintConfig: typeof eslint["default"];
+export const prettierConfig: typeof prettier["default"];
